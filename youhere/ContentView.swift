65ab5dcf2f495cf5cd365c04307b179c88dcc337
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2024-01-03 19:20:11 +0530

    Initial Commit

---
+++ youhere/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  youhere
+//
+//  Created by Akash Shanmugaraj on 03/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ youhere/youhereApp.swift
@@ -0,0 +1,17 @@
+//
+//  youhereApp.swift
+//  youhere
+//
+//  Created by Akash Shanmugaraj on 03/01/24.
+//
+
+import SwiftUI
+
+@main
+struct youhereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
